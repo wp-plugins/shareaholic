0a511a1f31aac78dc3ef8747fb9d43e0f2a16c5c
shareaholic <shareaholic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-02 15:07:16 +0000

    Tagging v7.0.0.4
    git-svn-id: https://plugins.svn.wordpress.org/shareaholic/tags/7.0.0.4@735092 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/script_snapengage.php
@@ -0,0 +1,17 @@
+<!-- begin SnapEngage code -->
+<script type="text/javascript">
+  (function() {
+    var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+    se.src = '//commondatastorage.googleapis.com/code.snapengage.com/js/62fa2e8b-38a9-4304-ba5c-86503444d30c.js';
+    var done = false;
+    se.onload = se.onreadystatechange = function() {
+      if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+        done = true;
+        // Place your SnapEngage JS API code below
+        //SnapEngage.openProactiveChat(true, true); // Example: open the proactive chat on load
+      }
+    };
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+  })();
+</script>
+<!-- end SnapEngage code -->
