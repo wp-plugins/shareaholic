e33971e5c4aa0f1da3e3ed8c0fd6966ba5c0f238
shareaholic <shareaholic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-12 15:12:07 +0000

    Tagging v7.4.0.7
    git-svn-id: https://plugins.svn.wordpress.org/shareaholic/tags/7.4.0.7@931223 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/why_to_sign_up.php
@@ -0,0 +1,25 @@
+<div class="signuppromo unit size1of5">
+<p class="promoh1"><?php echo sprintf(__('Sign up for a FREE Shareaholic account to unlock more features.', 'shareaholic')); ?></p>
+<ul>
+  <li><?php echo sprintf(__('Brand your social shares with your @Twitterhandle', 'shareaholic')); ?></li>
+  <li><?php echo sprintf(__('Pick your favorite URL shortener, including support for branded bitly short links.', 'shareaholic')); ?></li>
+  <li><?php echo sprintf(__('Monetize your site, plus lots more!', 'shareaholic')); ?></li>
+</ul>
+<button data-href='edit' id='general_settings' class="btn btn-large btn-success"><?php echo sprintf(__('Edit General Website Settings', 'shareaholic')); ?></button>
+
+<p class="signuppromo_note"><?php echo sprintf(__("Click the button above to sign up or log in to your free Shareaholic account. It takes just seconds to sign up and it's free!", 'shareaholic')); ?></p>
+</div>
+
+<div class="help_links unit size1of5">  
+  <ul>
+    <li><a href="http://support.shareaholic.com/" target="_blank"><?php _e('Shareaholic Helpdesk', 'shareaholic'); ?></a></li>
+    <li><a href="https://shareaholic.com/tools/wordpress/translations" target="_blank"><?php _e('Submit a Language Translation', 'shareaholic'); ?></a></li>
+    <li><a href="https://shareaholic.com/tools/browser/" target="_blank"><?php _e('Get the Shareaholic Browser Add-on', 'shareaholic'); ?></a></li>
+  </ul>
+</div>
+
+<div class="help_links unit size1of5">  
+  <div id="shrsb-updates">
+      <div id="shrsb-updates-container"></div>
+  </div>
+</div>
