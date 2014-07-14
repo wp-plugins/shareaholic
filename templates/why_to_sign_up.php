bff2c0cf584cc77072fd3557c5849a4fb9543ce8
shareaholic <shareaholic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-14 19:04:51 +0000

    Tagging v7.5.0.1
    git-svn-id: https://plugins.svn.wordpress.org/shareaholic/tags/7.5.0.1@948283 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/why_to_sign_up.php
@@ -0,0 +1,24 @@
+<div class="signuppromo unit size1of5">
+<p class="promoh1"><?php echo sprintf(__('Sign up for a FREE Shareaholic account to unlock more features.', 'shareaholic')); ?></p>
+<ul>
+  <li><?php echo sprintf(__('Get the Floated Share Buttons app', 'shareaholic')); ?></li>
+  <li><?php echo sprintf(__('Monetize your site, plus lots more!', 'shareaholic')); ?></li>
+</ul>
+<button data-href='edit' id='general_settings' class="btn btn-large btn-success"><?php echo sprintf(__('Unlock more features, for free', 'shareaholic')); ?></button>
+
+<p class="signuppromo_note"><?php echo sprintf(__("Click the button above to sign up or log in to your free Shareaholic account. It takes just seconds to sign up and it's free!", 'shareaholic')); ?></p>
+</div>
+
+<div class="help_links unit size1of5">  
+  <ul>
+    <li><a href="http://support.shareaholic.com/" target="_blank"><?php _e('Shareaholic Helpdesk', 'shareaholic'); ?></a></li>
+    <li><a href="https://shareaholic.com/tools/wordpress/translations" target="_blank"><?php _e('Submit a Language Translation', 'shareaholic'); ?></a></li>
+    <li><a href="https://shareaholic.com/tools/browser/" target="_blank"><?php _e('Get the Shareaholic Browser Add-on', 'shareaholic'); ?></a></li>
+  </ul>
+</div>
+
+<div class="help_links unit size1of5">  
+  <div id="shrsb-updates">
+      <div id="shrsb-updates-container"></div>
+  </div>
+</div>
