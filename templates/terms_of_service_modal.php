3eb9efb8af17fee6386b61250e37d616a1952d26
shareaholic <shareaholic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-19 14:06:25 +0000

    Tagging v7.6.0.0
    git-svn-id: https://plugins.svn.wordpress.org/shareaholic/tags/7.6.0.0@993278 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/terms_of_service_modal.php
@@ -0,0 +1,27 @@
+<div class='reveal-modal blocking-modal' id='terms_of_service_modal'>
+  <h4><?php echo sprintf(__('Thanks for Installing!', 'shareaholic')); ?></h4>
+  <div class="content pal">
+  <p><?php echo sprintf(__('%sShareaholic%s gives you the essential tools you need to become a successful publisher.', 'shareaholic'), '<strong>', '</strong>'); ?></p>
+  <div class="line pvl">
+  <div class="unit size1of3">
+    <img src="<?php echo $image_url ?>/sharebuttons@2x.png" alt="Share Buttons" width="200" height="200" />
+    <p><?php echo sprintf(__('%sShare buttons%s enables your readers to spread your content.', 'shareaholic'), '<strong>', '</strong>'); ?></p>
+  </div>
+  <div class="unit size1of3">
+    <img src="<?php echo $image_url ?>/related_content@2x.png" alt="Related Content" width="200" height="200" />
+    <p><?php echo sprintf(__('%sRelated content%s keeps people on your site and turns visitors into readers.', 'shareaholic'), '<strong>', '</strong>'); ?></p>
+  </div>
+  <div class="unit size1of3">
+    <img src="<?php echo $image_url ?>/analytics@2x.png" alt="Analytics" width="200" height="200" />
+    <p><?php echo sprintf(__('%sAnalytics%s gives you all the insights you need to grow your site.', 'shareaholic'), '<strong>', '</strong>'); ?></p>
+  </div>
+  </div>
+  <div class="pvl">
+    <a id='get_started' class="btn_main"><?php echo sprintf(__('Get Started »', 'shareaholic')); ?></a>
+    <p><small><?php echo sprintf(__('By clicking "Get Started" you agree to Shareholic\'s %sTerms of Service%s and %sPrivacy Policy%s.', 'shareaholic'), '<a href="https://shareaholic.com/terms/?src=wp_admin" target="_new">', '</a>', '<a href="https://shareaholic.com/privacy/?src=wp_admin" target="_new">', '</a>'); ?></small>
+</p>
+
+<p><small style="font-size:10px; line-height: 17px; display: block; font-weight:normal;"><?php echo sprintf(__('Note: Shareaholic incorporates trusted 3rd party services like Google Analytics to enhance  social analytics and service reliability, and is an integral part of how Shareaholic functions.', 'shareaholic')); ?> <?php echo sprintf(__('However, you can toggle this and any other setting under "Advanced Settings" at any time.', 'shareaholic')); ?></small></p>
+  </div>
+  </div>
+</div>
